bf17350f5a4a16f2f2e3146412000360998daa29
Leanderson Lima <leanderson.devlima@gmail.com>
2024-02-01 19:52:54 -0300

    teste

--- bancoDeDados.js
+++ bancoDeDados.js
@@ -5,7 +5,9 @@ async function conectaBancoDeDados() {
     try {
         console.log('Conexão com o banco de dados iniciou')
 
-        await mongoose.connect(process.env.MONGO_URL)
+        //await mongoose.connect(process.env.MONGO_URL)
+        await mongoose.connect('mongodb+srv://leandersondevlima:<8lsHkq5cJFE9HL0g>@clustermulheres.8lbmiby.mongodb.net/?retryWrites=true&w=majority')
+
 
         console.log('Conexão com o banco de dados feita com sucesso!')
     } catch(erro) {
